585fbd29931db7a87494195b6270382e6a0403e5
imanvakilian <imanvakiliangitu@gmail.com>
2024-12-15 10:36:22 +0330

    employee : (ref) update database models

---
+++ prisma/migrations/20241214064111_set_manager_id_field_to_optional_in_business_model/migration.sql
@@ -0,0 +1,8 @@
+-- DropForeignKey
+ALTER TABLE "common"."business" DROP CONSTRAINT "business_manager_id_fkey";
+
+-- AlterTable
+ALTER TABLE "common"."business" ALTER COLUMN "manager_id" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "common"."business" ADD CONSTRAINT "business_manager_id_fkey" FOREIGN KEY ("manager_id") REFERENCES "common"."manager"("employee_id") ON DELETE SET NULL ON UPDATE CASCADE;
